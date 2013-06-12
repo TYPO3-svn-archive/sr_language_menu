7452fd23a547e95f064be9413764f62ef56d7056
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-12 19:38:36 +0000

    Added feature #49074: Reconfigure the extension in ext_tables.php and ext_localconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@76519 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,32 @@
 <?php
-if (!defined ('TYPO3_MODE')) die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_srlanguagemenu_pi1.php', '_pi1', 'CType', 1);
+// Extending TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbBackend until http://forge.typo3.org/issues/47192 is resolved
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbBackend'] = array(
+    'className' => 'SJBR\SrLanguageMenu\Persistence\Generic\Storage\Typo3DbBackend'
+);
+
+// Include page TS configuration for new element wizard
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/modWizards.txt">');
+
+// Configuring the language menu plugin
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+	$_EXTKEY,
+	// A unique name of the plugin in UpperCamelCase
+	'LanguageMenu',
+	// An array holding the controller-action-combinations that are accessible
+	// The first controller and its first action will be the default
+	array (
+		'Menu' => 'index',
+	),
+	// An array of non-cachable controller-action-combinations (they must already be enabled)
+	array(
+		'Menu' => 'index',
+	),
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,31 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
+// Register language menu static templates
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/PluginSetup', 'Language Menu Setup');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/DefaultStyles', 'Language Menu CSS Styles');
+
+/**
+ * Registers the plugin to be listed in the Backend
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
+	$_EXTKEY,
+	// A unique name of the plugin in UpperCamelCase
+	'LanguageMenu',
+	// A title shown in the backend dropdown field
+	'LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xml:pi1_title'
+	//'Language selection menu'
+);
+if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
+	\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('tt_content');
+}
 $tempColumns = Array (
 	'tx_srlanguagemenu_languages' => Array (		
 		'exclude' => 0,		
-		'label' => 'LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.tx_srlanguagemenu_languages',		
+		'label' => 'LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.languages',		
 		'config' => Array (
 			'type' => 'group',
 			'internal_type' => 'db',
@@ -15,28 +36,29 @@ $tempColumns = Array (
 			'show_thumbs' => 1,
 		)
 	),
-	'tx_srlanguagemenu_type' => Array (        
+	'tx_srlanguagemenu_type' => array(        
 		'exclude' => 0,        
-		'label' => 'LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.tx_srlanguagemenu_type',        
-		'config' => Array (
+		'label' => 'LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.layout',        
+		'config' => array(
 			'type' => 'select',
-			'items' => Array (
-				Array('LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.tx_srlanguagemenu_type.I.0', '0'),
-				Array('LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.tx_srlanguagemenu_type.I.1', '1'),
-				Array('LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.tx_srlanguagemenu_type.I.2', '2'),
+			'items' => array(
+				array('LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.layout.I.0', '0'),
+				array('LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.layout.I.1', '1'),
+				array('LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.layout.I.2', '2'),
 			),
 		),
 	),
 );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns, 1);
 
-t3lib_div::loadTCA('tt_content');
-t3lib_extMgm::addTCAcolumns('tt_content',$tempColumns,1);
+$pluginSignature = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY)) . '_languagemenu';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/form.xml', $pluginSignature);
 
-$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2, tx_srlanguagemenu_type;;;;3-3-3,tx_srlanguagemenu_languages';
-$TCA['tt_content']['ctrl']['typeicons'][$_EXTKEY.'_pi1'] = t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif';
-
-t3lib_extMgm::addPlugin(Array('LLL:EXT:sr_language_menu/locallang_db.xml:tt_content.CType', $_EXTKEY.'_pi1'),'CType');
-
-if (TYPO3_MODE=='BE')	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_srlanguagemenu_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_srlanguagemenu_pi1_wizicon.php';
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general';
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] .= ', --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.headers;headers';
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] .= ',--div--;LLL:EXT:sr_language_menu/Resources/Private/Language/locallang.xlf:settings.title;;;;3-3-3, pi_flexform';
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] .= ',--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access';
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] .= ', --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames';
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicons'][$pluginSignature] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif';
 
 ?>
