34151e3a62f1701e9d775414dc85d1e425ac8e5d
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-12 19:18:33 +0000

    Added feature #49074: Add domain model for system languages and pages language overlays
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@76512 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Domain/Model/PageLanguageOverlay.php
@@ -0,0 +1,83 @@
+<?php
+namespace SJBR\SrLanguageMenu\Domain\Model;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * The Page Language Overlay model
+ */
+class PageLanguageOverlay extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+	
+	/**
+	 * Page
+	 *
+	 * @var \SJBR\SrLanguageMenu\Domain\Model\Page
+	 */
+	protected $page;	
+
+	/**
+	 * Language
+	 *
+	 * @var \SJBR\SrLanguageMenu\Domain\Model\SystemLanguage
+	 */
+	protected $language;
+
+	/**
+	 * Sets the page
+	 *
+	 * @return \SJBR\SrLanguageMenu\Domain\Model\Page $page
+	 * @return void
+	 */
+	public function setPage(\SJBR\SrLanguageMenu\Domain\Model\Page $page) {
+		$this->page = $page;
+	}	
+
+	/**
+	 * Returns the page
+	 *
+	 * @return \SJBR\SrLanguageMenu\Domain\Model\Page
+	 */
+	public function getPage() {
+		return $this->page;
+	}
+
+	/**
+	 * Sets the language
+	 *
+	 * @return \SJBR\SrLanguageMenu\Domain\Model\SystemLanguage $language
+	 * @return void
+	 */
+	public function setLanguage(\SJBR\SrLanguageMenu\Domain\Model\SystemLanguage $language) {
+		$this->language = $language;
+	}
+
+	/**
+	 * Returns the language
+	 *
+	 * @return \SJBR\SrLanguageMenu\Domain\Model\SystemLanguage
+	 */
+	public function getLanguage() {
+		return $this->language;
+	}
+}
+?>
---
+++ Classes/Domain/Repository/PageLanguageOverlayRepository.php
@@ -0,0 +1,59 @@
+<?php
+namespace SJBR\SrLanguageMenu\Domain\Repository;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Stanislas Rolland <typo3@sjbr.ca>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * The page language overlay repository
+ */
+class PageLanguageOverlayRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+	/**
+	 * Initialize the repository for unrestricted access to page language overlays
+	 *
+	 * @return void
+	 */	
+	public function __construct() {
+		parent::__construct();
+		$querySettings = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
+		$querySettings->setRespectStoragePage(FALSE);
+		$querySettings->setRespectSysLanguage(FALSE);
+		$this->setDefaultQuerySettings($querySettings);
+	}
+
+	/**
+	 * 
+	 *
+	 * @param \SJBR\SrLanguageMenu\Domain\Model\Page $page
+	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
+	 */	
+	public function findByPage(\SJBR\SrLanguageMenu\Domain\Model\Page $page) {
+		$query = $this->createQuery();
+		$query->matching(
+			$query->equals('pid', $page->getUid())
+		);
+		return $query->execute();
+	}
+}
+?>
