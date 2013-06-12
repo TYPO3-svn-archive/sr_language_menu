8eda42e88f28bc9c2614bf0b5cf9360d0b6aff84
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-12 19:32:52 +0000

    Added feature #49074: Change type of columns of old plugin in tt_content
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@76517 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,5 +4,5 @@
 
 CREATE TABLE tt_content (
 	tx_srlanguagemenu_type int(11) unsigned DEFAULT '0' NOT NULL,
-	tx_srlanguagemenu_languages tinyblob NOT NULL
+	tx_srlanguagemenu_languages tinytext NOT NULL
 );
