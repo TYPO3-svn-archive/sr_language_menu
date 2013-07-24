fe632fb2ab0288283a5b650441745befacb0bb59
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-24 14:23:50 +0000

    Require TYPO3 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@78295 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.0.6-6.1.99',
+			'typo3' => '6.0.7-6.1.99',
 			'static_info_tables' => '6.0.4-6.1.99',
 		),
 		'conflicts' => 
