5038e95f39c4180cb22229659e4df88672048aaa
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-24 14:21:41 +0000

    Revert fix to issue #49077: Extend Extbase class until issue #47192 is fixed in core
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@78294 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// Extending TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbBackend until http://forge.typo3.org/issues/47192 is resolved
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbBackend'] = array(
-    'className' => 'SJBR\SrLanguageMenu\Persistence\Generic\Storage\Typo3DbBackend'
-);
-
 // Include page TS configuration for new element wizard
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/modWizards.txt">');
 
