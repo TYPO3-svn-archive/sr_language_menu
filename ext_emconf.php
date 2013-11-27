84920d034bc021382ccc397f473128b53e279d4b
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-27 16:09:12 +0000

    Enable in TYPO3 6.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@81750 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.0.7-6.1.99',
-			'static_info_tables' => '6.0.4-6.1.99',
+			'typo3' => '6.0.7-6.2.99',
+			'static_info_tables' => '6.0.4-6.0.99',
 		),
 		'conflicts' => 
 		array (
