db48f9b30a64fc7dce77bff2b92854ff5995c2f0
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-25 19:40:11 +0000

    Update version to 6.0.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@84496 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "sr_language_menu".
  *
- * Auto generated 27-11-2013 16:12
+ * Auto generated 25-03-2014 15:38
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'A plugin to display a list of languages to select from. Clicking on a language links to the corresponding version of the page.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '6.0.13',
+	'version' => '6.0.4',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.0.7-6.2.99',
+			'typo3' => '6.0.13-6.2.99',
 			'static_info_tables' => '6.0.4-6.0.99',
 		),
 		'conflicts' => 
