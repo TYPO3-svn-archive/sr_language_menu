1e92f414f4c7496617446e901afa55ea56a5bb05
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-03-18 06:46:38 +0000

    Require TYPO3 CMS 6.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@88500 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'A plugin to display a list of languages to select from. Clicking on a language links to the corresponding version of the page.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '6.0.7',
+	'version' => '6.1.0',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -35,17 +35,14 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.0.13-6.2.99',
-			'static_info_tables' => '6.0.4-6.99.99',
+			'typo3' => '6.2.0-7.99.99',
+			'static_info_tables' => '6.0.7-'
 		),
 		'conflicts' => 
 		array (
 		),
 		'suggests' => 
 		array (
-		),
-	),
-	'_md5_values_when_last_written' => '',
+		)
+	)
 );
-
-?>
