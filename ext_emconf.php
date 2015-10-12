ec2f117e44be0aea6e5a07c4b51fd77ae974065b
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-12 22:27:11 +0000

    Require Static Info Tables 6.3.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_language_menu/trunk@89881 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,7 @@
 <?php
-/***************************************************************
- * Extension Manager/Repository config file for ext "sr_language_menu".
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+/*
+ * Extension Manager configuration file for ext "sr_language_menu".
+ */
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Language Selection',
@@ -13,9 +9,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'plugin',
 	'version' => '6.1.0',
 	'state' => 'stable',
-	'uploadfolder' => FALSE,
+	'uploadfolder' => 0,
 	'createDirs' => '',
-	'clearcacheonload' => FALSE,
+	'clearcacheonload' => 0,
 	'author' => 'Stanislas Rolland',
 	'author_email' => 'typo3(arobas)sjbr.ca',
 	'author_company' => 'SJBR',
@@ -23,11 +19,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.2.0-7.99.99',
-			'static_info_tables' => '6.2.1-'
+			'static_info_tables' => '6.3.1-6.3.99'
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		)
+		'conflicts' => array(),
+		'suggests' => array()
 	)
 );
